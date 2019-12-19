e384e74ed1e654643d20a680fb34442cfd746e88
zhoie <zhoie@live.com>
2019-12-19 19:46:55 +0800

    add instance of flask

---
+++ bugging_index/instance/config.py
@@ -0,0 +1,18 @@
+# jieba 分词时 排除项   一项一行
+DEL_WORDS = r"C:\Users\zhouhao\PycharmProjects\bugging_index\searchclient\static\jiebawords\del_word.txt"
+
+# 结巴分词 指定词汇
+ADD_WORDS = r"C:\Users\zhouhao\PycharmProjects\bugging_index\searchclient\static\jiebawords\add_word.txt"
+
+# 是否开启重建索引
+REBUILD = False
+
+# 是否允许备份
+BACKUP = False
+BACKUP_DIR = r"C:\Users\zhouhao\PycharmProjects\bugging_index\searchclient\static\csv"
+
+# 是否允许从CSV导入
+IMPORT_CSV = False
+
+# 允许还原db
+INIT = False
