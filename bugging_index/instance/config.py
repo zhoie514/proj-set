afd221541334135c64b432d1dc85fa159b1a4f72
zhouhao <zhoie@M70CHMAV.xwfintech.net>
2020-01-02 16:30:40 +0800

    nothing

--- bugging_index/instance/config.py
+++ bugging_index/instance/config.py
@@ -1,15 +1,15 @@
 # jieba 分词时 排除项   一项一行
-DEL_WORDS = r"C:\Users\zhouhao\github\proj-set\bugging_index\searchclient\static\jiebawords\del_word.txt"
+DEL_WORDS = "/home/zhoie/proj-set/bugging_index/searchclient/static/jiebawords/del_word.txt"
 
 # 结巴分词 指定词汇
-ADD_WORDS = r"C:\Users\zhouhao\github\proj-set\bugging_index\searchclient\static\jiebawords\add_word.txt"
+ADD_WORDS = "/home/zhoie/proj-set/bugging_index/searchclient/static/jiebawords/add_word.txt"
 
 # 是否开启重建索引
 REBUILD = True
 
 # 是否允许备份
 BACKUP = True
-BACKUP_DIR = r"C:\Users\zhouhao\github\proj-set\bugging_index\searchclient\static\csv"
+BACKUP_DIR = "/home/zhoie/proj-set/bugging_index/searchclient/static/csv"
 
 # 是否允许从CSV导入
 # IMPORT_CSV = True
